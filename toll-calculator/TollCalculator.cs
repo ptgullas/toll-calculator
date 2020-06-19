37a508188ad61a20261e2b459a165564986e2237
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-06-19 11:15:31 -0400

    Implement my initial version of TollCalculator

---
+++ toll-calculator/TollCalculator.cs
@@ -0,0 +1,31 @@
+﻿using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+using System;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Text;
+
+namespace toll_calculator {
+    public class TollCalculator {
+        public double CarToll { get; set; }
+        public double TaxiToll { get; set; }
+        public double BusToll { get; set; }
+        public double DeliveryTruckToll { get; set; }
+        public TollCalculator(double carToll = 2, double taxiToll = 3.5, double busToll = 5, double deliveryTruckToll = 10) {
+            CarToll = carToll;
+            TaxiToll = taxiToll;
+            BusToll = busToll;
+            DeliveryTruckToll = deliveryTruckToll;
+        }
+
+        public double Calculate(object vehicle) {
+            if (vehicle is Car c) { return CarToll; }
+            else if (vehicle is Taxi t) { return TaxiToll; }
+            else if (vehicle is Bus b) { return BusToll; }
+            else if (vehicle is DeliveryTruck d) { return DeliveryTruckToll; }
+            else
+                throw new ArgumentOutOfRangeException($"{nameof(vehicle)} is not one of the supported vehicles!");
+        }
+    }
+}
