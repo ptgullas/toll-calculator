b01a4a1fe56011d9d947f6e81aef9f7a02de87cb
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-06-19 12:04:18 -0400

    Implement additional toll requirements

--- toll-calculator/TollCalculator.cs
+++ toll-calculator/TollCalculator.cs
@@ -22,9 +22,22 @@ namespace toll_calculator {
         public decimal CalculateToll(object vehicle) =>
             vehicle switch
             {
-                Car c => CarToll,
-                Taxi t => TaxiToll,
+                Car { Passengers: 0 } => CarToll + 0.5m,
+                Car { Passengers: 1 } => CarToll,
+                Car { Passengers: 2 } => CarToll - 0.5m,
+                Car c => CarToll - 1,
+
+                Taxi { Fares: 0 } => TaxiToll + 0.5m,
+                Taxi { Fares: 1 } => TaxiToll,
+                Taxi { Fares: 2 } => TaxiToll - 0.5m,
+                Taxi t => TaxiToll - 1,
+
+                Bus b when ((double) b.Riders / (double) b.Capacity) < 0.5 => BusToll + 2m,
+                Bus b when ((double)b.Riders / (double)b.Capacity) > 0.9 => BusToll - 1m,
                 Bus b => BusToll,
+
+                DeliveryTruck dt when (dt.GrossWeightClass > 5000) => DeliveryTruckToll + 5,
+                DeliveryTruck dt when (dt.GrossWeightClass < 3000) => DeliveryTruckToll - 2,
                 DeliveryTruck dt => DeliveryTruckToll,
                 { } => throw new ArgumentOutOfRangeException(message: "This is not one of the supported vehicles!", paramName: nameof(vehicle)),
                 null => throw new ArgumentNullException(nameof(vehicle))
